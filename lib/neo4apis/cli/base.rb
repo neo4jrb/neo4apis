34845ae33c3070d93526aa012db67f61894b26d5
Brian Underwood <ml+github@semi-sentient.com>
2015-03-27 21:34:00 +0100

    Make a neo4j_session method for the Thor Base class and introduce username and password options

--- lib/neo4apis/cli/base.rb
+++ lib/neo4apis/cli/base.rb
@@ -2,6 +2,12 @@ module Neo4Apis
   module CLI
     class Base < ::Thor
       class_option :neo4j_url, type: :string,  default: 'http://localhost:7474'
+      class_option :neo4j_username, type: :string
+      class_option :neo4j_password, type: :string
+
+      def neo4j_session
+        Neo4j::Session.open(:server_db, options[:neo4j_url], basic_auth: {username: options[:neo4j_username], password: options[:neo4j_password]})
+      end
     end
   end
 end
